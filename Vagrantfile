467c14c81d4b1c705eb5e2c13289294844a93e09
Ona KE code machine <tech+ke@ona.io>
2017-09-15 14:24:21 +0300

    dd Tranquility and increase RAM

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,14 @@
 Vagrant.configure(2) do |config|
   config.vm.define :druid do |druid|
+    druid.vm.network "forwarded_port", guest: 8090, host: 8090
+    druid.vm.network "forwarded_port", guest: 8081, host: 8081
+    druid.vm.network "forwarded_port", guest: 8082, host: 8082
+    druid.vm.network "forwarded_port", guest: 1088, host: 1088
+    druid.vm.network "forwarded_port", guest: 8200, host: 8200
     druid.vm.network "private_network", ip: "192.168.50.4"
     druid.vm.box = "ubuntu/trusty64"
     druid.vm.provider :virtualbox do |v|
-        v.customize ["modifyvm", :id, "--memory", "4048"]
+        v.customize ["modifyvm", :id, "--memory", "8192"]
         v.customize ["modifyvm", :id, "--cpus", "2"] # druid overlord requires multi-core machine
         v.customize ["modifyvm", :id, "--ioapic", "on"]
     end
--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #! /bin/bash -e
-
+export DEBIAN_FRONTEND=noninteractive
 echo "Updating apt-get"
-apt-get update
+apt-get -qqy update
 
 echo "Installing prerequisites"
 apt-get install -y supervisor vim less net-tools inetutils-ping curl git telnet nmap socat dnsutils netcat software-properties-common maven
 
 echo "Adding WebUpd8 repository"
 add-apt-repository -y ppa:webupd8team/java
-apt-get update
+apt-get -qqy update
 
 echo "Installing JDK 7"
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-DEBIAN_FRONTEND=noninteractive apt-get install -y oracle-java7-installer oracle-java7-set-default
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections 
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -y install oracle-java8-installer
 
 echo "Downloading Zookeeper"
 curl http://mirrors.ibiblio.org/apache/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -o zookeeper-3.4.6.tar.gz
@@ -23,10 +23,17 @@ mv zookeeper-3.4.6 zookeeper
 cp zookeeper/conf/zoo_sample.cfg zookeeper/conf/zoo.cfg
 
 echo "Downloading Druid"
-curl -O http://static.druid.io/artifacts/releases/druid-0.9.0-bin.tar.gz
-tar -xzf druid-0.9.0-bin.tar.gz
+curl -O http://static.druid.io/artifacts/releases/druid-0.10.1-bin.tar.gz
+tar -xzf druid-0.10.1-bin.tar.gz
 rm -rf druid
-mv druid-0.9.0 druid
+mv druid-0.10.1 druid
+chown -R vagrant:vagrant druid
+
+echo "Downloading Tranquility"
+curl -O http://static.druid.io/tranquility/releases/tranquility-distribution-0.8.2.tgz
+tar -xzf tranquility-distribution-0.8.2.tgz
+rm -rf tranquility
+mv tranquility-distribution-0.8.2 druid/tranquility
 chown -R vagrant:vagrant druid
 
 echo "Starting Services"
